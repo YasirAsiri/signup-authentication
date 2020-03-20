87b80c64b5cf2e99bb5b056745435d190decb555
Yasir Asiri <iyasser89@gmail.com>
2020-03-20 11:10:02 +1000

    encryption and env variables added

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,8 +1,11 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
 
 const app = express();
 
@@ -14,12 +17,14 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true})
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
     email: String,
     password: String
-};
+});
 
-const User = new mongoose.model("User", userSchema);
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+
+const User = new mongoose.model("User", userSchema); 
 
 
 app.get('/', (req, res) => {
@@ -48,6 +53,24 @@ app.post('/register', (req, res) => {
         }
     })
 });
+
+app.post('/login', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    User.findOne({email: username}, (err, foundUser) => {
+        if (err){
+            console.log(err);
+            
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password){
+                    res.render('secrets');
+                }
+            }
+        }
+    })
+})
 
 
 app.listen(3000, () => {
